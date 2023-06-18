02afa29a06496aaa86426614f4ce40c332ec96e6
iisyos <kosuke1216a@yahoo.co.jp>
2023-06-18 12:10:11 +0900

    *feat: get Contributors

--- src/services/publish-issue.ts
+++ src/services/publish-issue.ts
@@ -6,14 +6,25 @@ export class PublishIssue {
     private bodyBuilder: IssueBodyBuilder
   ) {}
   async publish(): Promise<void> {
-    const body = await this.bodyBuilder.buildBody(['iisyos'])
-    const issue = {
+    // const body = await this.bodyBuilder.buildBody(['iisyos'])
+    // const issue = {
+    //   owner: 'iisyos',
+    //   repo: 'actions_runner',
+    //   title: 'New Issue',
+    //   body
+    // }
+    // const {data} = await this.octokit.rest.issues.create(issue)
+    // console.log('Created issue: %s', data.html_url)
+    this.getContributors()
+  }
+  private async getContributors(): Promise<void> {
+    const {data} = await this.octokit.rest.repos.listContributors({
       owner: 'iisyos',
-      repo: 'actions_runner',
-      title: 'New Issue',
-      body
-    }
-    const {data} = await this.octokit.rest.issues.create(issue)
-    console.log('Created issue: %s', data.html_url)
+      repo: 'actions_runner'
+    })
+    const contributoers = data
+      .filter(contributor => contributor.type === 'User')
+      .map(contributor => contributor.login)
+    console.log(contributoers)
   }
 }
